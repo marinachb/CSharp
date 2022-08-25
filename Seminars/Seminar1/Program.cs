86a2768ab235380753c7cdfe578d18f1f2c40011
Marina <chernobaymarina@yandex.ru>
2022-08-26 02:23:39 +0300

    add task1

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+// Задача 1. Напишите, программу, которая на вход принимает число и выдает его квадрат
+
+int num;
+
+Console.Write("Input a number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int quad = num * num;
+
+//Console.WriteLine("The square of " + num+ " is " + quad);
+
+Console.WriteLine($"The square of {num} is {quad}");
+*/
+
+// Задача 2. Напишите программу, которая на вход принимает два чила и проверяет является ли первое число квадратом второго
+
+
