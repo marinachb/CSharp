564cc8713007e9342f40c3ee0311d4f3209874dc
Marina <chernobaymarina@yandex.ru>
2022-08-26 02:34:07 +0300

    add task2

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -15,4 +15,18 @@ Console.WriteLine($"The square of {num} is {quad}");
 
 // Задача 2. Напишите программу, которая на вход принимает два чила и проверяет является ли первое число квадратом второго
 
+Console.Write("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
 
+int quad2 = n2 * n2;
+
+if(n1 == quad2)
+{
+    Console.WriteLine($"{n1} is quad of {n2}");
+}
+else
+{
+    Console.WriteLine($"{n1} is not quad of {n2}");
+}
